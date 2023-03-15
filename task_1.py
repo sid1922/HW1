a15e44df7945bf50178f0d60f0307200509c597e
sid1922 <111591366+sid1922@users.noreply.github.com>
2023-03-15 20:58:32 +0300

    hw1

---
+++ task_1.py
@@ -0,0 +1,27 @@
+# i = input("Введите трехзначное число: ")
+# print(int(i[0])+int(i[1])+int(i[2]))
+
+
+# a = int(input("Введите число: "))
+# b = a
+# c = (a+b)*2
+# S = a+b+c
+# print(S)
+
+
+# i = input("Введите шестизначное число: ")
+# s1 = int(i[0])+int(i[1])+int(i[2])
+# s2 = int(i[3])+int(i[4])+int(i[5])
+# if s1 == s2:
+#     print("yes")
+# else:
+#     print("no")
+
+
+# n = int(input("Введите число "))
+# m = int(input("Введите число "))
+# k = int(input("Введите число "))
+# if k < n*m and ((k % n == 0) or (k % m == 0)):
+#     print("yes")
+# else:
+#     print("no")
